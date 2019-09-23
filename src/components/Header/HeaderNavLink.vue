8aed899044525ae7682057f6660ecb914e0f2856
Javi Puche <javierpuchevictoria@gmail.com>
2019-09-23 14:43:08 +0200

    cambios

--- src/components/Header/HeaderNavLink.vue
+++ src/components/Header/HeaderNavLink.vue
@@ -1,12 +1,15 @@
 <template>
     <a :href="href" class="c-header__nav-link">
         <span class="c-header__nav-link-text">{{ text }}</span>
-        <Icon v-if="icon" :icon="icon" class="c-header__nav-link-icon" />
+        <div class="c-header__nav-link-icon">
+            <Icon v-if="icon" :icon="icon" />
+        </div>
     </a>
 </template>
 
 <script>
     import Icon from '@/components/Icon/Icon.vue'
+
     export default {
         components: {
             Icon
--- src/pages/muchoviaje/Home.vue
+++ src/pages/muchoviaje/Home.vue
@@ -1,6 +1,48 @@
 <template>
     <Layout brand="muchoviaje">
-        home muchoviaje
+        <br><br><br><br><br>
+
+        <div class="c-navigation c-wrapper c-wrapper--l">
+            <nav class="c-navigation__nav c-wrapper__inner">
+                <ul class="c-navigation__list">
+                    <li class="c-navigation__item">
+                        <a href="#" class="c-navigation__link">Menu1</a>
+                    </li>
+                    <li class="c-navigation__item">
+                        <a href="#" class="c-navigation__link">Menu2</a>
+                    </li>
+                    <li class="c-navigation__item">
+                        <a href="#" class="c-navigation__link">Menu3</a>
+                        <nav class="c-navigation__nav">
+                            <ul class="c-navigation__list">
+                                <li class="c-navigation__item">
+                                    <a href="#" class="c-navigation__link">Menu1</a>
+                                    <nav class="c-navigation__nav">
+                                        <ul class="c-navigation__list">
+                                            <li class="c-navigation__item">
+                                                <a href="#" class="c-navigation__link">Menu1</a>
+                                            </li>
+                                            <li class="c-navigation__item">
+                                                <a href="#" class="c-navigation__link">Menu2</a>
+                                            </li>
+                                            <li class="c-navigation__item">
+                                                <a href="#" class="c-navigation__link">Menu3</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </li>
+                                <li class="c-navigation__item">
+                                    <a href="#" class="c-navigation__link">Menu2</a>
+                                </li>
+                                <li class="c-navigation__item">
+                                    <a href="#" class="c-navigation__link">Menu3</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </Layout>
 </template>
 
